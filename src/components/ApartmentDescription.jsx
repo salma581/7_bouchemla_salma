39dc8e9cac302f0d241b66cdcc1c4bcafc649b68
salma bouchemla <salmabouchemla@gmail.com>
2025-02-06 14:03:22 +0100

    creation apartment description

---
+++ src/components/ApartmentDescription.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import "./ApartmentDescription.scss";
+export function ApartmentDescription() {
+    return (
+
+);
+}
--- src/pages/ApartmentPage.jsx
+++ src/pages/ApartmentPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./ApartmentPage.scss";
 
 function ApartmentPage() {
     return (
@@ -6,11 +7,58 @@ function ApartmentPage() {
             <div>
                 <img src='https://picsum.photos/800/400' alt='apartment'/>
             </div>
-            <h1 className='apartment-page__title'></h1>
-            <h2 className='apartment-page__subtitle'></h2>
-            <p>Cozy</p>
-            <p>Canal</p>
-            <p>Paris 10</p>
+            <div className="apartment__header">
+                <div className="apartment__title">
+              <h1>Crazy loft on Canal Sain Martin</h1>
+              <h2>Paris, Ile de France</h2>
+             <div className="apartment__tags">
+                 <span>Cozy</span>
+                 <span>Canal</span>
+                 <span>Paris 10</span> 
+             </div>
+             </div>
+               <div className="apartment__owner">
+                  <div className="apartment__owner__détails">
+                     <h3 ><span>Alexandre</span><span>Dumas</span> </h3>
+                     <div className="apartment__owner__badge"></div>
+                 </div> 
+              
+                 <div className="apartment__owner__stars">
+                     <span className="on">★</span>
+                     <span className="on">★</span>
+                     <span className="on">★</span>
+                     <span className="off">★</span>
+                     <span className="off">★</span>
+                  </div>
+              </div>
+            </div>
+            <div className="apartment__description">
+                <p className="description__header">
+                   <span> Description</span>
+                   <i class="fas fa-chevron-down"></i>
+                    </p>
+                <p className="description__content">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, 
+                   deserunt quidem. Sed facere reprehenderit beatae numquam corporis harum provident
+                   cum doloremque eaque ullam. Non deleniti architecto voluptatum expedita eum,
+                   atque quae magnam quasi ut quia fugiat laudantium in aspernatur temporibus,
+                   ad dolorum veniam neque, excepturi dignissimos quam voluptatem! Labore, repellat!
+               </p>
+            </div>
+            <div className="apartment__description">
+                <p className="description__header">
+                   <span> Description</span>
+                   <i class="fas fa-chevron-down"></i>
+              </p>
+                <p className="description__content">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, 
+                   deserunt quidem. Sed facere reprehenderit beatae numquam corporis harum provident
+                   cum doloremque eaque ullam. Non deleniti architecto voluptatum expedita eum,
+                   atque quae magnam quasi ut quia fugiat laudantium in aspernatur temporibus,
+                   ad dolorum veniam neque, excepturi dignissimos quam voluptatem! Labore, repellat!
+               </p>
+               
+            </div>
         </div>
     );
 }
