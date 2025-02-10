d58e1de260b3550d9833867e74ce683c19cb5089
salma bouchemla <salmabouchemla@gmail.com>
2025-02-10 17:21:20 +0100

    apartgrid et apartcard integration du json

--- src/components/ApartmentCard.jsx
+++ src/components/ApartmentCard.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import "./ApartmentCard.scss";
 import { Link } from "react-router-dom";
 
-function ApartmentCard() {
+function ApartmentCard(props) {
+  console.log("props dans un apartment card:",props)
 return (
   <Link to= "/flat"> 
   <div className='apartment'>
-    <div className='apartment__title'>Titre de la location</div> 
+    <img src= {props.imageUrl} alt='' />
+    <div className='apartment__title'>{props.title} </div> 
     </div>
     </Link> 
 );
--- src/components/ApartmentGrid.jsx
+++ src/components/ApartmentGrid.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./ApartmentGrid.scss";
 import ApartmentCard from "./ApartmentCard.jsx"
 
+
 function ApartmentGrid() {
+  const [apartments, setApartments]= useState([]);
+  useEffect(fetchApartments, []);
+  //useEffect avec une array vide == exécute cette fonction au chergement du composant
+  function fetchApartments(){
+    fetch("db.json")
+ .then((res) => res.json())
+ .then((res) => setApartments(res))
+ .catch(console.error)
+  }
+  
   return (
     <div className="grid">
-      <ApartmentCard/>
-      <ApartmentCard/>
-      <ApartmentCard/>
-      <ApartmentCard/>
+     {apartments.map((apartment)=> (
+      <ApartmentCard 
+      title={apartment.title} 
+      imageUrl={apartment.cover}
+      />
+     ))}
+     
     </div>
   );
 }
